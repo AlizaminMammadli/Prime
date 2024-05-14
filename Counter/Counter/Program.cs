e38c155318c144b412c7a72730d6184490824058
Alizamin23 <alizaminam@code.edu.az>
2024-05-14 04:30:43 +0400

    Add files

---
+++ Counter/Counter/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Counter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = -000200;
+            string numbStr = number.ToString();
+
+            if (number < 0)
+            {
+                numbStr = numbStr.Substring(1);
+                Console.WriteLine(number + " " + "consists of" + " " + numbStr.Length + " " + "character");
+            }
+
+            else
+            {
+                Console.WriteLine(number + " " + "consists of" + " " + numbStr.Length + " " + "character");
+
+            }
+        }
+    }
+}
---
+++ Sade ve murekkeb/Sade ve murekkeb/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace Sade_ve_murekkeb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 23;
+
+
+            if (number <= 1)
+            {
+                Console.WriteLine(number + " " + "is neither prime nor composite");
+            }
+            else if (number == 2 || number == 3 || number == 5 || number == 7)
+            {
+                Console.WriteLine(number + " " + "is prime");
+            }
+            else if (number % 2 == 0 || number % 3 == 0 || number % 5 == 0 || number % 7 == 0)
+            {
+                Console.WriteLine(number + " " + "is composite");
+            }
+
+            else
+            {
+                Console.WriteLine(number + " " + "is prime");
+            }
+
+
+
+        }
+    }
+}
